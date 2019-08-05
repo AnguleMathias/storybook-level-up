15c97137df8039623389f3e0c487401da0be6539
AnguleMathias <angulemathias3@gmail.com>
2019-08-05 16:24:36 +0300

    theming

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,16 @@
-import { configure } from '@storybook/react';
+import { addParameters, configure } from '@storybook/react';
+import crfTheme from './crfTheme';
 
 import "../bootstrap-grid.css";
 import "../bootstrap-reboot.min.css";
 import "../bootstrap.css";
 import "../main.css";
 
+addParameters({
+  options: {
+    theme: crfTheme
+  }
+});
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /\.stories\.js$/);
 function loadStories() {
